ad5162f7b99695ad39b22983c8d066bf561b00f8
William de Assis <148707118+WillAssisTJ@users.noreply.github.com>
2024-07-02 13:35:30 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Pessoa {
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nome: %s, Data de Nascimento: %s", nome, dataNascimento);
+    }
 }
